82e32979fce96c3c10f3e39ded23d22a7fd7cbb2
Elyse Salberg <elyse_salberg@putnam.com>
2016-09-21 11:44:21 -0400

    Return uucp,news.crit and boot logging to RedHat; remove redundant emerg logging

--- manifests/params.pp
+++ manifests/params.pp
@@ -100,18 +100,18 @@ class rsyslog::params {
         $omit_local_logging     = false
       }
       elsif versioncmp($::operatingsystemmajrelease, '6') == 0 {
-        $rsyslog_package_name   = 'rsyslog'
-        $mysql_package_name     = 'rsyslog-mysql'
-        $pgsql_package_name     = 'rsyslog-pgsql'
-        $gnutls_package_name    = 'rsyslog-gnutls'
-        $relp_package_name      = 'rsyslog-relp'
-        $default_config_file    = 'rsyslog_default'
-        $modules                = [
+        $rsyslog_package_name                = 'rsyslog'
+        $mysql_package_name                  = 'rsyslog-mysql'
+        $pgsql_package_name                  = 'rsyslog-pgsql'
+        $gnutls_package_name                 = 'rsyslog-gnutls'
+        $relp_package_name                   = 'rsyslog-relp'
+        $default_config_file                 = 'rsyslog_default'
+        $modules                             = [
           '$ModLoad imuxsock # provides support for local system logging',
           '$ModLoad imklog   # provides kernel logging support (previously done by rklogd)',
           '#$ModLoad immark  # provides --MARK-- message capability',
         ]
-        $omit_local_logging     = false
+        $omit_local_logging                  = false
       }
       elsif versioncmp($::operatingsystemmajrelease, '7') >= 0 {
         $rsyslog_package_name                = 'rsyslog'
